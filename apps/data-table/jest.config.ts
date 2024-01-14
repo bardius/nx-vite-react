2f3d58e7b6e188aeb11c9e7be8e8fc14fc12750c
George Bardis <george@bardis.info>
2024-01-14 04:56:18 +0200

    ci(monorepo): fix path to app coverage report

--- apps/data-table/jest.config.ts
+++ apps/data-table/jest.config.ts
@@ -7,5 +7,5 @@ export default {
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: 'reports/data-table',
+  coverageDirectory: 'reports/coverage',
 };
