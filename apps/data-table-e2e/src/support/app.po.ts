e4ae6ce14051d0d9dc5654888f00238d44627b90
George Bardis <george@bardis.info>
2024-01-20 02:41:55 +0200

    test(data-table): add cypress tests

--- apps/data-table-e2e/src/support/app.po.ts
+++ apps/data-table-e2e/src/support/app.po.ts
@@ -1,1 +1,5 @@
-export const getGreeting = () => cy.get('h1');
+export const getHeading = () => cy.get('.saltText-h2');
+export const getTable = () => cy.get('[data-testid="table"]');
+export const getRows = () => cy.get('[data-testid="table"] tbody tr');
+export const getColumns = () => cy.get('[data-testid="table"] tbody td');
+export const getAssetClassSortBtn = () => cy.get('[data-testid="assetClass_action_sort_btn"]');
